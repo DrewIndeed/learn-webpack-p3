d83f094df80071944d2ad8ea365dfe87bba75dcd
Andrew Le <s3820098@rmit.edu.vn>
2022-05-23 11:19:31 +0700

    4. Entry and output config as usual

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // dist can be any name
+    filename: 'main.js', // main can be any name
+  },
+};
