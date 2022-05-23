28c6144fc8283c89c86f9d2b81b9a63d85fc18ab
Andrew Le <s3820098@rmit.edu.vn>
2022-05-23 10:56:33 +0700

    2. install webpack + refactor to before using webpack
    - Meaning not having export + import yet

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
-import { inputsAreValid } from './utils/inputs-are-valid';
-import { parseInputs } from './utils/parse-inputs';
-export const run = (alertService, componentService) => {
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+ const run = (alertService, componentService) => {
   alertService.hideErrors();
 
   componentService.onClick(() => {
@@ -16,3 +17,5 @@ export const run = (alertService, componentService) => {
     }
   });
 };
+
+run(alertService, componentService);
